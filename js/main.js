fa343624682c7850c244e563dbc0e96fd6d1709c
Leszek Janczewski <leszek.janczewski@gmail.com>
2017-10-21 10:40:58 +0200

    Dodanie pozostałych plików z repozytorium

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$('.hamburger').on('click', function() {
+  // if ($('.menu').hasClass('open')) {
+  //   $('.menu').removeClass('open');
+  // } else {
+  //   $('.menu').addClass('open');
+  // }
+  $('.menu').toggleClass('open');
+});
